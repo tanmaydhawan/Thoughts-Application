a9dad08e82e4c71ee1ab220e91e1d48e81eca2bb
tanmaydhawan <tanmaydhawan11@gmail.com>
2025-04-06 19:01:28 +0530

    First step to create something Special!

---
+++ src/main/java/com/tanmay/Thought_App/ThoughtAppApplication.java
@@ -0,0 +1,13 @@
+package com.tanmay.Thought_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanmay/Thought_App/ThoughtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmay.Thought_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
